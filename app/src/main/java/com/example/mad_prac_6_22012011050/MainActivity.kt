95c665f38a46978bf83c5363aa52663c32a2cc42
Bansi <bansivachhani153@gmail.com>
2024-11-13 09:17:16 +0530

    IA

--- app/src/main/java/com/example/mad_prac_6_22012011050/MainActivity.kt
+++ app/src/main/java/com/example/mad_prac_6_22012011050/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.mad_prac_6_22012011050
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +15,14 @@ class MainActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
             insets
         }
+        val heartImage = findViewById<ImageView>(R.id.heart)
+        val heartAnimation=heartImage.background as AnimationDrawable
+        heartAnimation.start()
+        val alarmImage = findViewById<ImageView>(R.id.alarm_imageview)
+        val alarmAnimation=alarmImage.background as AnimationDrawable
+        alarmAnimation.start()
     }
 }
